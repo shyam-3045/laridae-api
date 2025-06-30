d9b127990f96dfb6d8329dd87aeb567bc019d98d
shyam-3045 <s.m.shyam45@gmail.com>
2025-06-30 19:33:36 +0530

    Initial setup and connected to Db completed

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require("express")
 const app=express()
 const cors=require("cors")
+const dotenv=require("dotenv")
+const ConnectDb = require("./config/DbConnection")
+dotenv.config()
 
-const PORT = 5000 || process.env.PORT
+const PORT =  process.env.PORT||5000
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors())
+
+ConnectDb()
 
 app.get("/",(req,res)=>
 {
