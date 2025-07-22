1730c7d07ee819874a8e36adeb84dab126cd1661
shyam-3045 <s.m.shyam45@gmail.com>
2025-07-22 16:25:58 +0530

    otp done

--- controllers/otp.js
+++ controllers/otp.js
@@ -5,16 +5,15 @@ const otpStore = new Map();
 const generateOTP = () =>
   Math.floor(100000 + Math.random() * 900000).toString();
 
-
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
   },
-  tls:{
-    rejectUnauthorized:false
-  }
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 exports.sendOtp = async (req, res) => {
@@ -24,7 +23,10 @@ exports.sendOtp = async (req, res) => {
       return sendError(res, "Email Reuired", 400);
     }
     const otp = generateOTP();
-    const expiry = Date.now() + 5 * 60 * 1000;
+    const expiry = Date.now() + 1 * 60 * 1000;
+    const user = otpStore.get(email);
+    if (user) {
+    otpStore.delete(email)}
     otpStore.set(email, { otp, expiry });
     await transporter.sendMail({
       from: process.env.MAIL_USER,
@@ -32,10 +34,9 @@ exports.sendOtp = async (req, res) => {
       subject: "Your OTP Code",
       text: `Your OTP code is ${otp}. It will expire in 5 minutes.`,
     });
-    
+
     return sendSuccess(res, "Otp sent to User", 200);
   } catch (error) {
-    
     return sendError(res, "Internal server Error", 500, {
       error: error.message,
     });
@@ -46,27 +47,24 @@ exports.verifyOtp = async (req, res) => {
   try {
     const { email, otp } = req.body;
     if (!email || !otp) {
-        return sendError(res,"Missing required Credentials",400)
-
+      return sendError(res, "Missing required Credentials", 400);
     }
     const saved = otpStore.get(email);
-    console.log(saved)
-    if(!saved)
-    {
-        return sendError(res,"No Otp found for this email",400)
+    console.log(saved);
+    if (!saved) {
+      return sendError(res, "No Otp found for this email", 400);
     }
 
-
     if (Date.now() > saved.expiry) {
+      otpStore.delete(email);
+      return sendError(res, "Otp has expired", 400);
+    }
+
+    if (saved.otp !== otp) {
+      return sendError(res, "Invalid otp", 400);
+    }
+
     otpStore.delete(email);
-    return sendError(res,"Otp has expired",400)
-  }
-
-  if (saved.otp !== otp) {
-    return sendError(res,"Invalid otp",400);
-  }
-
-  otpStore.delete(email); 
-  return sendSuccess(res,"Otp verfied Successfully",200)
+    return sendSuccess(res, "Otp verfied Successfully", 200);
   } catch (error) {}
 };
