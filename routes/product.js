0b19f579d518091218feca5c65fd3d9511098672
shyam-3045 <s.m.shyam45@gmail.com>
2025-11-08 16:05:44 +0530

    add image funcitionality added

--- routes/product.js
+++ routes/product.js
@@ -1,8 +1,9 @@
 const express=require("express")
-const { addProduct, getAllProducts } = require("../controllers/product")
+const { addProduct, getAllProducts, addproductImage } = require("../controllers/product")
 const router=express.Router()
 
 router.post("/addProducts",addProduct)
 router.get("/getProducts",getAllProducts)
+router.post("/addproductImage",addproductImage)
 
 module.exports=router
