cf6ae1127bec90e57bf77b7390b79a37310f733b
shyam-3045 <s.m.shyam45@gmail.com>
2026-02-24 00:25:03 +0530

    Admin Api's added

--- routes/product.js
+++ routes/product.js
@@ -1,9 +1,12 @@
 const express=require("express")
-const { addProduct, getAllProducts, addproductImage } = require("../controllers/product")
+const { addProduct, getAllProducts, addproductImage,deleteProduct, editProducts } = require("../controllers/product")
 const router=express.Router()
 
 router.post("/addProducts",addProduct)
 router.get("/getProducts",getAllProducts)
 router.post("/addproductImage",addproductImage)
+
+router.delete("/delectProduct",deleteProduct)
+router.post("/editProducts/:id",editProducts)
 
 module.exports=router
