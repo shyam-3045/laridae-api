6c85cbb12fcd539369713ea98a646246e65d7d19
shyam-3045 <s.m.shyam45@gmail.com>
2026-02-24 10:42:50 +0530

    Edit porduct api changed

--- routes/userInfo.js
+++ routes/userInfo.js
@@ -1,9 +1,10 @@
 const express=require("express")
-const { addUserDetails, editUserDetails, getUserDetails } = require("../controllers/userInfo")
+const { addUserDetails, editUserDetails, getUserDetails, getAlluserdetails } = require("../controllers/userInfo")
 const router=express.Router()
 
 router.post('/addUserDet',addUserDetails)
 router.post('/editUserDet',editUserDetails)
 router.post('/getUserDet',getUserDetails)
+router.get("/getAlluserDet",getAlluserdetails)
 
 module.exports=router
